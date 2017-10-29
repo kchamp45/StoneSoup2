0447465280b6c775b2ac1bc80ae105077863b3ce
Kimberly Lu <kimlu4@comcast.net>
2017-10-28 18:32:50 -0700

    display savedSoupList correctly after editing FBSoupVH to list out in same format as in SoupListAdapter.

--- app/src/main/java/com/epicodus/stonesoup/adapters/FirebaseSoupViewHolder.java
+++ app/src/main/java/com/epicodus/stonesoup/adapters/FirebaseSoupViewHolder.java
@@ -35,10 +35,12 @@ public class FirebaseSoupViewHolder extends RecyclerView.ViewHolder implements V
 
     public void bindSoup(Soup soup) {
         TextView nameTextView = (TextView) mView.findViewById(R.id.soupNameTextView);
-//        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+        TextView prepTextView = (TextView) mView.findViewById(R.id.prepTextView);
 
         nameTextView.setText(soup.getName());
-
+        ratingTextView.setText("Rating: " + soup.getRating() + "/5");
+        prepTextView.setText("Prep Time: " + soup.getPrepTime() + " minutes");
     }
 
     @Override
